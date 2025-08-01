76b50fd0a7c925a1386ccf74e5e9ecd4e3caed68
Cariad Eccleston <cariad@cariad.earth>
2025-08-01 08:37:11 +0000

    Deploy a hosted zone

---
+++ scripts/bootstrap-website.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -euo pipefail
+
+domain="${1}"
+
+safe_domain="${domain//./}"
+stack_name="httphostdev-website-${safe_domain}-bootstrap"
+
+aws cloudformation deploy \
+  --parameter-overrides \
+      "ApexDomain=${domain}" \
+  --stack-name    "${stack_name}" \
+  --template-file ./cloudformation/website-bootstrap.cf.yml
+
+echo
+echo "IMPORTANT: Update the name servers of ${domain} to:"
+echo
+
+aws cloudformation describe-stacks \
+  --output     text \
+  --query      "Stacks[0].Outputs[?(@.OutputKey == 'NameServers')] | [0].OutputValue" \
+  --stack-name "${stack_name}"
