aac111240edc3d169fc31dbe180c12e060f5b110
Cariad Eccleston <cariad@cariad.earth>
2025-08-01 14:52:22 +0000

    Add CloudFront distribution

--- scripts/deploy-website-infrastructure.sh
+++ scripts/deploy-website-infrastructure.sh
@@ -11,6 +11,7 @@ fi
 
 name_safe_domain="${domain//./}"
 stack_name_prefix="httphostdev-website-${name_safe_domain}"
+global_stack_name="${stack_name_prefix}-global"
 
 hosted_zone_id=$(
   aws route53 list-hosted-zones \
@@ -23,13 +24,22 @@ hosted_zone_id="${hosted_zone_id##*/}"
 aws cloudformation deploy \
   --parameter-overrides "ApexDomain=${domain}" \
                         "HostedZoneId=${hosted_zone_id}" \
-  --stack-name          "${stack_name_prefix}-global" \
+  --stack-name          "${global_stack_name}" \
   --tags                "httphost.dev:domain=${domain}" \
   --template-file       ./cloudformation/website-global.cf.yaml \
   --region              us-east-1
 
+certificate_arn=$(
+  aws cloudformation describe-stacks \
+    --output     text \
+    --query      "Stacks[0].Outputs[?(@.OutputKey == 'CertificateArn')] | [0].OutputValue" \
+    --region     us-east-1 \
+    --stack-name "${global_stack_name}"
+)
+
 aws cloudformation deploy \
   --parameter-overrides "ApexDomain=${domain}" \
+                        "CertificateArn=${certificate_arn}" \
   --stack-name          "${stack_name_prefix}-regional" \
   --tags                "httphost.dev:domain=${domain}" \
   --template-file       ./cloudformation/website-regional.cf.yaml
